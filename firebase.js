8aa09ebd10ff841635bcf2aa18c19fa09866951f
Pajar <92008208+Fjnesaa96@users.noreply.github.com>
2025-07-13 05:26:43 +0700

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCyXfGd92WDVlKapqTtfUAlElfwICxjzNc",
   authDomain: "indocapital-21d1f.firebaseapp.com",
@@ -8,3 +12,9 @@ const firebaseConfig = {
   appId: "1:434461925094:web:c104a84f62a887efd0e527",
   measurementId: "G-F62Q0YETB5"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export const firebaseObj = { auth, db, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, doc, setDoc, getDoc };
